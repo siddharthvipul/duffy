815c27dbf373ee1f1e0e1c36611d15509b8fec68
siddharthvipul <siddharthvipul1@gmail.com>
2019-09-05 19:36:20 +0530

    import models to initialize

--- duffy/models/__init__.py
+++ duffy/models/__init__.py
@@ -1,1 +1,3 @@
-from .nodes import Host, HostSchema, Session, SessionSchema, Project, SSHKey
+from .nodes import HostSchema, Session, SessionSchema, Project, SSHKey
+from .openstack_nodes import openstack_host
+from .baremetal_nodes import Host
